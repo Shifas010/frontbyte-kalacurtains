1623b568c420d86ae9d67d4be9c18ef5b4011c48
Shifasa010 <shifaskottayi@gmail.com>
2022-08-16 23:57:58 +0530

    edit home pag e discription add links

---
+++ script.js
@@ -0,0 +1,16 @@
+const name = document.getElementById("fname")
+const email = document.getElementById("email")
+
+const mobile = document.getElementById("mobile")
+const form = document.getElementById("subject")
+const errorElement = dociment.getElementById(' error')
+  var name_error = document.getElementById('name_error')
+ 
+
+var whatsapp = getElementsByClassName("whatsapp");
+whatsapp.addEventListener("click",pop);
+function pop() {
+    window.alert("Hey you want contact with our team ")
+    
+}
+pop()
